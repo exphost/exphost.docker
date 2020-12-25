cd524fcd7440178091d5516d1b6f97aafadcd04b
Marcin TORGiren Fabrykowski <torgiren@gmail.com>
2020-12-25 18:54:47 +0100

    migrate tests to new flow
    Signed-off-by: Marcin TORGiren Fabrykowski <torgiren@gmail.com>
    Change-Id: I5f44335c499c5a8f65101885bd4f941f638b46c9
    Signed-off-by: Marcin TORGiren Fabrykowski <torgiren@gmail.com>

--- tests/centos7/dependency.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-ansible-galaxy install -p roles -r requirements.yml
-[ -L roles/exphost.docker ] && unlink roles/exphost.docker
-[[ ! -L roles/exphost.docker && -d roles/exphost.docker ]] && rm -r roles/exphost.docker
-ln -s ../../../ roles/exphost.docker
--- tests/centos7/lint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-result=0
-trap 'result=1' ERR
-yamllint roles/exphost.docker/
-ansible-lint dummy_playbook.yml
-exit $result
-
